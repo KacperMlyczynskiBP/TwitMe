42a817648bdca99b390555386a2eed538ddb1e80
basillvista <basillvista@gmail.com>
2023-03-02 16:08:19 +0100

    JobsForTrendsAndSportsResults

---
+++ app/Jobs/ListNBADataJob.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Jobs;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Http\Request;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+
+class ListNBADataJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+            $date = Carbon::now()->format('Y-m-d');
+            $response = Http::withHeaders([
+            'x-rapidapi-host' => 'v1.basketball.api-sports.io',
+            'x-rapidapi-key' => config('services.rapid.key'),
+             ])->get("https://v1.basketball.api-sports.io/games?date=$date");
+            $results = collect($response->json(['response']))->slice(0,10);
+            Cache::remember('NBAResults',8600, function() use($results){
+                return $results;
+            });
+    }
+}
---
+++ app/Jobs/ListTrendsJob.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Post;
+use App\Services\TrendService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+
+class ListTrendsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+            $posts = Post::all();
+            $trends = [];
+
+            foreach ($posts as $post) {
+                $words = explode(' ', $post->body);
+                foreach ($words as $word) {
+                    $word = strtolower(preg_replace('/[^A-Za-z0-9\-]/', '', $word));
+                    if (strlen($word) > 2) {
+                        if (isset($trends[$word])) {
+                            $trends[$word]++;
+                        } else {
+                            $trends[$word] = 1;
+                        }
+                    }
+                }
+            }
+
+            arsort($trends);
+
+            $trends = array_slice($trends, 0, 10);
+
+            Cache::remember('trends', 86400, function ($trends) {
+               return $trends;
+            });
+
+    }
+}
